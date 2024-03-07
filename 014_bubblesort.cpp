0542066f1c192fe3bf366bf6b68afd4bdba659f3
ahsanhf <ahsanhf@gmail.com>
2024-03-07 20:32:01 +0700

    prosedur input

--- 014_bubblesort.cpp
+++ 014_bubblesort.cpp
@@ -5,3 +5,12 @@ using namespace std;
 int arr[20];           //deklarasi array a dengan ukuran 20
 int n;                 //deklarasi variabel n untuk menyimpan banyaknya elemen
 
+void input() {         //prosedur untuk input
+	while (true) {
+		cout << "masukan banyaknya elemen pada array : ";   //output ke layar
+		cin >> n; //input dari pengguna
+		if (n <= 20) //jika n kurang dari atau sama dengan 20
+			break; //keluar dari loop
+		else {     //jika n lebih dari 20
+			cout << "\narray dapat mempunyai maksimal 20 elemen.\n"; //output ke layar
+
