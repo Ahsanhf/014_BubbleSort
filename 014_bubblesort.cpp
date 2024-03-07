4942381fb88dedda52e4434f6165f6a0eb6097d9
ahsanhf <ahsanhf@gmail.com>
2024-03-07 20:33:52 +0700

    mengurutkan array

--- 014_bubblesort.cpp
+++ 014_bubblesort.cpp
@@ -23,4 +23,16 @@ void input() {         //prosedur untuk input
 	for (int i = 0; i < n; i++) {
 		cout << "data ke-" << (i + 1) << ": ";
 		cin >> arr[i];
+	}
+}
+void bubbleshortarray() { // prosedur untuk mengurutkan array dengan metode bubble short
+	int pass = 1; // step 1
+	do {
+		for (int j = 0; j <= n - 1 - pass; j++) { //step 2
+			if (arr[j] > arr[j + 1]) {            //step 3
+				int temp = arr[j];
+				arr[j] = arr[j + 1];
+				arr[j + 1] = temp;
+			}
+		}
 
